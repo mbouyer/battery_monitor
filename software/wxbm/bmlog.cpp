5906fdc11bc6ab88d04d85628625f772faa0faaf
Manuel Bouyer <bouyer@antioche.eu.org>
2022-07-03 19:52:57 +0200

    work in progress on log: store all log entries in a std::vector; update when new entries are available

--- software/wxbm/bmlog.cpp
+++ software/wxbm/bmlog.cpp
@@ -27,6 +27,7 @@
 
 #include <wx/wx.h>
 #include <iostream>
+#include <err.h>
 #include <N2K/NMEA2000.h>
 #include "bmstatus.h"
 #include "bmlog.h"
@@ -35,51 +36,79 @@ bmLog::bmLog(wxWindow* parent)
 	: wxFrame(parent, wxID_ANY, _T("bmLog"))
 {
 	cur_log_entry = 0;
-	log_req_state = LOG_IDLE;
+	log_req_state = LOG_REQ_IDLE;
 	log_tx = (private_log_tx *)nmea2000P->get_frametx(nmea2000P->get_tx_bypgn(PRIVATE_LOG));
 	bmsizer = new wxFlexGridSizer(4, 5, 5);
+	if ((errno = pthread_mutex_init(&log_mtx, NULL)) != 0)
+		err(1, "init log_mtx");
+	log_state = LOG_INIT;
 }
 
 void
 bmLog::address(int a)
 {
-	if (log_req_state == LOG_IDLE) {
+	log_lock();
+	if (log_req_state == LOG_REQ_IDLE && log_state == LOG_INIT) {
 		log_req.cmd = PRIVATE_LOG_REQUEST_FIRST;
 		log_req.sid++;
 		log_req.idx = 0;
-		log_req_state = LOG_REQ;
+		log_req_state = LOG_REQ_DOREQ;
+		log_update_state = LOG_UP_DOUP;
 	}
-
+	log_unlock();
 }
 
 void
 bmLog::addLogEntry(int sid, double volts, double amps,
                  int temp, int instance, int idx, bool last)
 {
-	if (log_req_state != LOG_WAIT_BLOCK || log_req.sid != sid)
+	if (log_req_state != LOG_REQ_WAIT_BLOCK || log_req.sid != sid)
 		return; /* not waiting for that */
 	wxASSERT_MSG(cur_log_entry < LOG_ENTRIES && cur_log_entry >= 0,
 	    wxString::Format("cur_log_entry %d", cur_log_entry));
-	log_entries[cur_log_entry].volts = volts;
-	log_entries[cur_log_entry].amps = amps;
-	log_entries[cur_log_entry].temp = temp;
-	log_entries[cur_log_entry].instance = instance;
-	log_entries[cur_log_entry].idx = idx;
+	received_log_entries[cur_log_entry].volts = volts;
+	received_log_entries[cur_log_entry].amps = amps;
+	received_log_entries[cur_log_entry].temp = temp;
+	received_log_entries[cur_log_entry].instance = instance;
+	received_log_entries[cur_log_entry].id = (idx << ID_IDX_SHIFT) |
+	    (cur_log_entry << ID_INDEX_SHIFT);
+	received_log_entries[cur_log_entry].time = 0;
 	cur_log_entry++;
 	if (last) {
+		log_lock();
 		for (int i = 0; i < cur_log_entry; i++) {
-			printf("sid 0x%02x idx 0x%02x %3d inst %2d volts %2.2f amps %3.3f temp %3d\n",
-			    sid, log_entries[i].idx, i,
-			    log_entries[i].instance,
-			    log_entries[i].volts, log_entries[i].amps,
-			    log_entries[i].temp);
+			printf("sid 0x%02x idx 0x%06x %3d inst %2d volts %2.2f amps %3.3f temp %3d",
+			    sid, received_log_entries[i].id, i,
+			    received_log_entries[i].instance,
+			    received_log_entries[i].volts, received_log_entries[i].amps,
+			    received_log_entries[i].temp);
+			switch(log_update_state) {
+			case LOG_UP_IDLE:
+				wxASSERT_MSG(false, _T("log_update_state idle"));
+				break;
+			case LOG_UP_SEARCH:
+				if (received_log_entries[i].id ==
+				     log_entries.back().id) {
+					printf(" found\n");
+					/* up to now these are new entries */
+					log_update_state = LOG_UP_DOUP;
+				} else {
+					printf("\n");
+				}
+				break;
+			case LOG_UP_DOUP:
+				printf(" store\n");
+				log_entries.push_back(received_log_entries[i]);
+				break;
+			}
 		}
 		cur_log_entry = 0;
 		log_req.cmd = PRIVATE_LOG_REQUEST_NEXT;
 		log_req.sid++;
 		log_req.idx = idx;
-		log_req_state = LOG_REQ;
+		log_req_state = LOG_REQ_DOREQ;
 		// sendreq();
+		log_unlock();
 	} else {
 		wxASSERT_MSG(cur_log_entry < LOG_ENTRIES && cur_log_entry >= 0,
 		    wxString::Format("cur_log_entry %d", cur_log_entry));
@@ -89,31 +118,44 @@ bmLog::addLogEntry(int sid, double volts, double amps,
 void
 bmLog::logError(int sid, int err)
 {
-	if (log_req_state != LOG_WAIT_BLOCK || log_req.sid != sid)
+	if (log_req_state != LOG_REQ_WAIT_BLOCK || log_req.sid != sid)
 		return; /* not for us */
+	log_lock();
 	switch(err) {
 	case PRIVATE_LOG_ERROR_NOTFOUND:
+		wxASSERT(log_req.cmd == PRIVATE_LOG_REQUEST);
 		printf("log idx 0x%x not found\n", log_req.idx);
-		log_req_state = LOG_IDLE;
+		/* assume the log was reset */
+		log_req.cmd = PRIVATE_LOG_REQUEST_FIRST;
+		log_req.sid++;
+		log_req.idx = 0;
+		log_req_state = LOG_REQ_DOREQ;
+		log_update_state = LOG_UP_DOUP;
 		break;
 	case PRIVATE_LOG_ERROR_LAST:
 		wxASSERT(log_req.cmd == PRIVATE_LOG_REQUEST_NEXT);
-		log_req_state = LOG_IDLE;
+		log_req_state = LOG_REQ_IDLE;
+		log_update_state = LOG_UP_IDLE;
 		printf("log complete\n");
+		if (log_state == LOG_INIT)
+			log_state = LOG_IDLE;
+		gettimeofday(&last_data, NULL);
 		break;
 	}
+	log_unlock();
 }
 
 void
 bmLog::tick(void)
 {
+	log_lock();
 	switch(log_req_state) {
-	case LOG_IDLE:
+	case LOG_REQ_IDLE:
 		break;
-	case LOG_REQ:
+	case LOG_REQ_DOREQ:
 		sendreq();
 		break;
-	case LOG_WAIT_BLOCK:
+	case LOG_REQ_WAIT_BLOCK:
 	    {
 		struct timeval now, diff;
 		gettimeofday(&now, NULL);
@@ -128,4 +170,21 @@ bmLog::tick(void)
 		break;
 	    }
 	}
+	if (log_state == LOG_IDLE && log_req_state == LOG_REQ_IDLE) {
+		struct timeval now, diff;
+		gettimeofday(&now, NULL);
+		timersub(&now, &last_data, &diff);
+		if (diff.tv_sec >= 60) {
+			/* request new data */
+			log_req.cmd = PRIVATE_LOG_REQUEST;
+			log_req.sid++;
+			log_req.idx =
+			  (log_entries.back().id & ID_IDX_MASK) >> ID_IDX_SHIFT;
+			log_req_state = LOG_REQ_DOREQ;
+			log_update_state = LOG_UP_SEARCH;
+			printf("request new from 0x%04x\n", log_req.idx);
+			sendreq();
+		}
+	}
+	log_unlock();
 }
